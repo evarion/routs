90e4a1be59b8a0f580ec213cf52b402d6500fc0b
Aleksandr <gmreloj@gmail.com>
2022-06-17 17:00:56 +0300

    JSplitPane

--- src/main/java/com/evarion/gui/MainGuiNew1.java
+++ src/main/java/com/evarion/gui/MainGuiNew1.java
@@ -16,26 +16,20 @@ public class MainGuiNew1 extends JFrame {
     JPanel jPanelRightDataDown = new JPanel();
     JPanel jPanelDownPanelInfo = new JPanel();
 
-    JPanel jPanelEmpty = new JPanel();
     JPanel jPanelLogo = new JPanel();
-    JButton jButton1 = new JButton("Text");
-    JButton jButton2 = new JButton("Text");
-    JButton jButton3 = new JButton("Text");
-    JButton jButton4 = new JButton("Text");
-    JButton jButton5 = new JButton("Text");
-    JButton jButton6 = new JButton("Text");
-    JButton jButton7 = new JButton("Text");
+
+    JLabel logo = new JLabel("LOGO");
 
 
-    JButton[] buttonsTopMenu = new JButton[11];
-    JButton[] buttonsRightsMenu = new JButton[6];
+    JButton[] buttonsTopMenu = new JButton[12];
+    JButton[] buttonsRightsMenu = new JButton[7];
     JButton[] buttonsLeftMenu = new JButton[8];
 
     public MainGuiNew1() {
         super("App");
         createGUI();
     }
-    
+
 
     public void createGUI() {
         //MainPanel.createPanel();
@@ -46,15 +40,26 @@ public class MainGuiNew1 extends JFrame {
 
         addPanel();
         addButtonOnPanel();
-
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //setResizable(false);
+
     }
 
 
     public void addPanel() {
-
         setLayout(new GridBagLayout());
         GridBagConstraints constraints1 = new GridBagConstraints();
+
+        int heightYellowPanel = 80;
+        int magentaHeight = 760;
+        int widthOrange = 120;
+        int widthMagentaGreen = 1200;
+        int widthCyanRedBlue = 500;
+        int heightGreenBlue = 100;
+        int heightRedCyanGroupPanel = magentaHeight/2;
+        int heightOrange = magentaHeight+heightYellowPanel+heightGreenBlue;
+        int widthYellow=widthMagentaGreen+widthCyanRedBlue;
+
 
         constraints1.weightx = 0;
         constraints1.weighty = 0;
@@ -62,7 +67,7 @@ public class MainGuiNew1 extends JFrame {
         constraints1.gridy = 0;
         constraints1.gridheight = 5;
         constraints1.gridwidth = 1;
-        jPanelLeft.setPreferredSize(new Dimension(120, 820));
+        jPanelLeft.setPreferredSize(new Dimension(widthOrange, heightOrange)); //(new Dimension(120, 820));
         jPanelLeft.setBackground(Color.ORANGE);
         add(jPanelLeft, constraints1);
 
@@ -72,19 +77,9 @@ public class MainGuiNew1 extends JFrame {
         constraints1.gridy = 0;
         constraints1.gridheight = 1;
         constraints1.gridwidth = 1;
-        jPanelCentre.setPreferredSize(new Dimension(1000, 100));
+        jPanelCentre.setPreferredSize(new Dimension(widthMagentaGreen, heightGreenBlue)); //(new Dimension(1100, 100));
         jPanelCentre.setBackground(Color.green);
         add(jPanelCentre, constraints1);
-
-        constraints1.weightx = 0;
-        constraints1.weighty = 0;
-        constraints1.gridx = 2;
-        constraints1.gridy = 0;
-        constraints1.gridheight = 1;
-        constraints1.gridwidth = 1;
-        jPanelRight.setPreferredSize(new Dimension(400, 100));
-        jPanelRight.setBackground(Color.BLUE);
-        add(jPanelRight, constraints1);
 
         constraints1.weightx = 0;
         constraints1.weighty = 0;
@@ -92,9 +87,19 @@ public class MainGuiNew1 extends JFrame {
         constraints1.gridy = 2;
         constraints1.gridheight = 2;
         constraints1.gridwidth = 1;
-        jPanelData.setPreferredSize(new Dimension(1000, 640));
+        jPanelData.setPreferredSize(new Dimension(widthMagentaGreen, magentaHeight)); //(new Dimension(1100, 640));
         jPanelData.setBackground(Color.MAGENTA);
         add(jPanelData, constraints1);
+
+        constraints1.weightx = 0;
+        constraints1.weighty = 0;
+        constraints1.gridx = 2;
+        constraints1.gridy = 0;
+        constraints1.gridheight = 1;
+        constraints1.gridwidth = 1;
+        jPanelRight.setPreferredSize(new Dimension(widthCyanRedBlue, heightGreenBlue)); //(new Dimension(300, 100));
+        jPanelRight.setBackground(Color.BLUE);
+        add(jPanelRight, constraints1);
 
         constraints1.weightx = 0;
         constraints1.weighty = 0;
@@ -102,7 +107,7 @@ public class MainGuiNew1 extends JFrame {
         constraints1.gridy = 2;
         constraints1.gridheight = 1;
         constraints1.gridwidth = 1;
-        jPanelRightDataUp.setPreferredSize(new Dimension(400, 320));
+        jPanelRightDataUp.setPreferredSize(new Dimension(widthCyanRedBlue, heightRedCyanGroupPanel)); //(new Dimension(300, 320));
         jPanelRightDataUp.setBackground(Color.cyan);
         add(jPanelRightDataUp, constraints1);
 
@@ -112,7 +117,7 @@ public class MainGuiNew1 extends JFrame {
         constraints1.gridy = 3;
         constraints1.gridheight = 1;
         constraints1.gridwidth = 1;
-        jPanelRightDataDown.setPreferredSize(new Dimension(400, 320));
+        jPanelRightDataDown.setPreferredSize(new Dimension(widthCyanRedBlue, heightRedCyanGroupPanel)); //(new Dimension(300, 320));
         jPanelRightDataDown.setBackground(Color.RED);
         add(jPanelRightDataDown, constraints1);
 
@@ -122,7 +127,7 @@ public class MainGuiNew1 extends JFrame {
         constraints1.gridy = 4;
         constraints1.gridheight = 0;
         constraints1.gridwidth = 2;
-        jPanelDownPanelInfo.setPreferredSize(new Dimension(1400, 80));
+        jPanelDownPanelInfo.setPreferredSize(new Dimension(widthYellow, heightYellowPanel)); //(new Dimension(1400, 80));
         jPanelDownPanelInfo.setBackground(Color.YELLOW);
         add(jPanelDownPanelInfo, constraints1);
     }
@@ -133,15 +138,17 @@ public class MainGuiNew1 extends JFrame {
         GridBagConstraints constraints2 = new GridBagConstraints();
         constraints2.anchor = GridBagConstraints.NORTH;
         constraints2.weighty = 0.001;
+        constraints2.ipady = 30;
 
-        jPanelEmpty.setPreferredSize(new Dimension(0, 1));
-        constraints2.gridx = 0;
-        constraints2.gridy = 0;
-        jPanelLeft.add(jPanelEmpty, constraints2);
+        logo.setBorder(BorderFactory.createEmptyBorder( 35 , 0, 0, 0 ));
+        jPanelLogo.setPreferredSize(new Dimension(100, 70));
 
-        jPanelLogo.setPreferredSize(new Dimension(100, 75));
         constraints2.gridx = 0;
         constraints2.gridy = 1;
+
+
+        jPanelLogo.add(logo);
+
         jPanelLeft.add(jPanelLogo, constraints2);
 
 
@@ -149,13 +156,13 @@ public class MainGuiNew1 extends JFrame {
             buttonsLeftMenu[i] = new JButton();
             buttonsLeftMenu[i].setName("jButtonLeft" + i);
             buttonsLeftMenu[i].setText("Text");
-            buttonsLeftMenu[i].setPreferredSize(new Dimension(80, 60));
+            buttonsLeftMenu[i].setPreferredSize(new Dimension(100, 60));
         }
 
         int numY = 2;
         for (JButton jButton : buttonsLeftMenu) {
             constraints2.gridy = numY;
-            constraints2.gridx=0;
+            constraints2.gridx = 0;
             numY++;
             jPanelLeft.add(jButton, constraints2);
         }
@@ -183,7 +190,7 @@ public class MainGuiNew1 extends JFrame {
         constraints4.weightx = 0.001;
 
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < buttonsRightsMenu.length; i++) {
             buttonsRightsMenu[i] = new JButton();
             buttonsRightsMenu[i].setName("JButtonRight" + i);
             buttonsRightsMenu[i].setText("Text");
